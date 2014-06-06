2820ae0a20734283bc5cdc3fbfef37284b9430d2
Administrateur <Administrateur@192.168.2.105>
2014-06-06 11:52:56 +0200

    correction userController

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -22,29 +22,29 @@ class UserController extends Zend_Controller_Action
     public function createAction()
     {
 
-        if($this->getRequest()->isPost()){
+        //if($this->getRequest()->isPost()){
             //Récupération des données
             $data = $this->getRequest()->getPost();
 
-                $user = new Application_Model_User();
+            $user = new Application_Model_User();
 
-            $user->setId($data['id_user'])
-                ->setEmail_user($data['mail_user'])
-                ->setNom_user($data['nom_user'])
-                ->setPrenom_user($data['prenom_user'])
-                ->setAdresse_1_user($data['adresse_1_user'])
-                ->setAdresse_2_user($data['adresse_2_user'])
-                ->setZip_user($data['zip_user'])
-                ->setPassword_user($data['password_user'])
-                ->setNb_max_emprunt_user($data['nb_max_emprunt_user'])
-                ->setDelais_emprunt_user($data['delais_max_emprunt_user'])
-                ->setService_user($data['service_user'])
-                ->setDate_inscription($data['date_inscription'])
-                ->setBureau_user($data['bureau_user'])
-                ->setParution_id_parution($data['parution_id_user'])
-                ->setActif_user($data['actif_user'])
-                ->setValid_mail_user($data['valid_mail_user'])
-                ->setActivation_user($data['activation_user']);
+            $user->setId('1'/*$data['id_user']*/)
+                ->setEmail_user('billy@orange.fr'/*$data['mail_user']*/)
+                ->setNom_user('wallace'/*$data['nom_user']*/)
+                ->setPrenom_user('grommit'/*$data['prenom_user']*/)
+                ->setAdresse_1_user('1 rue Albert'/*$data['adresse_1_user']*/)
+                ->setAdresse_2_user(''/*$data['adresse_2_user']*/)
+                ->setZip_user('69380'/*$data['zip_user']*/)
+                ->setPassword_user('0000'/*$data['password_user']*/)
+                ->setNb_max_emprunt_user('3'/*$data['nb_max_emprunt_user']*/)
+                ->setDelais_emprunt_user('24'/*$data['delais_max_emprunt_user']*/)
+                ->setService_user(''/*$data['service_user']*/)
+                ->setDate_inscription('27/01/2014'/*$data['date_inscription']*/)
+                ->setBureau_user(''/*$data['bureau_user']*/)
+                ->setParution_id_parution(''/*$data['parution_id_user']*/)
+                ->setActif_user(''/*$data['actif_user']*/)
+                ->setValid_mail_user(''/*$data['valid_mail_user']*/)
+                ->setActivation_user(''/*$data['activation_user']*/);
 
             //Instance du Mapper
             $userMapper = new Application_Model_UserMapper();
@@ -55,7 +55,7 @@ class UserController extends Zend_Controller_Action
             $this->view->success = 'Enregistrement effectué';
 
 
-        }
+        //}
 
     }
 
