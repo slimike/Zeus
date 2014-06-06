d8e35b2e33b6e7d73c990859fe853bfd03e92760
Administrateur <Administrateur@192.168.2.113>
2014-06-06 09:38:48 +0200

    Installation de ZFDEBUG

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,7 +1,23 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+
+    protected function _initZFDebug() {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('ZFDebug');
+
+        $options = array(
+            'plugins' => array('Variables',
+                'Database' => array('adapter' => $db),
+                'File' => array('basePath' => '/path/to/project'),
+                'Cache' => array('backend' => $cache->getBackend()),
+                'Exception')
+        );
+        $debug = new ZFDebug_Controller_Plugin_Debug($options);
+
+        $this->bootstrap('frontController');
+        $frontController = $this->getResource('frontController');
+        $frontController->registerPlugin($debug);
+    }
 
 }
-
