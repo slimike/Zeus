9ba4265281056bb0a1df73ff8fbf341dbf5ad56b
Administrateur <Administrateur@WIN-6IF6UV4FN2J>
2014-07-11 12:44:46 +0200

    Màj CarrouselController

--- application/controllers/CarrouselController.php
+++ application/controllers/CarrouselController.php
@@ -1,7 +1,22 @@
 <?php
-class CarrouselController extends Zend_Controller_Action
+
+class CarouselController extends Zend_Controller_Action
 {
 	public function indexAction()
 	{
+		$carouselMapper = new Application_Model_CarouselMapper();
+		$carousel = $carouselMapper->fetchAll();
+
+		$this->view->assign('carousel', $carousel);
 	}
+
+	public function readAction()
+	{
+		$params = $this->getRequest()->getParams();
+
+		$carouselMapper = new Application_Model_CarouselMapper();
+		$carousel = $carouselMapper->find($params['id']);
+
+		$this->view->assign("carousel", $carousel);
+	}	
 }
